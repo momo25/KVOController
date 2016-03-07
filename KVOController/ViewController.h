1b47952d6a196356ea364841f0fc282c381e5d17
MomoFeng <era8989@163.com>
2016-03-07 11:41:46 +0800

    Initial Commit

---
+++ KVOController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOController
+//
+//  Created by 云冯 on 16/3/7.
+//  Copyright © 2016年 冯云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVOController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVOController
+//
+//  Created by 云冯 on 16/3/7.
+//  Copyright © 2016年 冯云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
